ccfc751c89488d90826f080276daf78ccc322f69
BENDE ANEESH <168645645+aneeshc1@users.noreply.github.com>
2024-07-11 11:37:38 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "aneesh-bucket" # Replace with your actual S3 bucket name
-    key    = "AKIA5FTZENJC2SL2OY5P"
+    key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
 }
